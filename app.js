8cd86d24650970625db1365eb10fb81e5096dbf3
rvstowrn <rvstowrn@gmail.com>
2020-04-25 01:05:28 +0530

    anurag version

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.carousel').carousel();
+    $('.parallax').parallax();
+	$('.tooltipped').tooltip();
+});
