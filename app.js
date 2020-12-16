4fb33af36e00f23a71e77d2a9ccb313c419515e1
Rishabh Verma <rvstowrn@gmail.com>
2020-12-16 20:33:59 +0530

    updated ui

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
     $('.carousel').carousel();
     $('.parallax').parallax();
 	$('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
 });
+
+const fab_btn = document.getElementById('fab_btn');
+fab_btn.style.display = "none";
+window.onscroll = ()=> {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        fab_btn.style.display = "unset";
+    } else {
+        fab_btn.style.display = "none";
+    }
+}
